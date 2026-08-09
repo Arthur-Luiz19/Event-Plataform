b3fc29b0ccd5b18f98d199c0029b5c415eb9e3e6
Arthur Luiz <arthur.luiz11@hotmail.com>
2026-08-09 12:08:50 -0300

    git commit -m "chore: initialize backend project"

---
+++ src/test/java/com/example/PlataformaEventos/PlataformaEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlataformaEventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
