91b26b93f38cc41621e0a00d8dff5ea763cf1a4a
Arthur Luiz <arthur.luiz11@hotmail.com>
2026-08-09 12:09:13 -0300

    git commit -m "chore: configure postgresql and flyway"

---
+++ src/main/resources/db/migration/V1__create_initial_schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users
+(
+    id         UUID PRIMARY KEY,
+    name       VARCHAR(100) NOT NULL,
+    email      VARCHAR(150) NOT NULL UNIQUE,
+    password   VARCHAR(255) NOT NULL,
+    role       VARCHAR(30)  NOT NULL,
+    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT chk_users_role
+        CHECK (role IN ('ROLE_ORGANIZER', 'ROLE_CLIENT', 'ROLE_GATE'))
+);
